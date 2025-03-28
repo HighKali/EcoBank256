1b96a2e7a2fbcfdcbb17877bb0c9381b679aae28
High Kali <163762069+HighKali@users.noreply.github.com>
2025-03-28 07:17:48 +0100

    Create CryptoNote.h
    Added include/CryptoNote.h

---
+++ include/CryptoNote.h
@@ -0,0 +1,13 @@
+#ifndef CRYPTONOTE_H
+#define CRYPTONOTE_H
+
+#include <boost/variant.hpp>
+#include <string>
+#include <vector>
+
+namespace CryptoNote {
+    using BlockHeight = uint32_t;
+    using TransactionId = uint32_t;
+}
+
+#endif
